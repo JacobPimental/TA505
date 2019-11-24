1f1070f204025ecd19ea9268675dcc5daa1fdc39
Jacob <jacob16682@gmail.com>
2019-11-23 23:17:47 -0500

    clean code

--- extract_shellcode.py
+++ extract_shellcode.py
@@ -10,20 +10,20 @@ def tohex(x):
 
 r2 = r2pipe.open("mal1.dll")
 
+# Grab encrypted data located at 0x10004640
 encrypted_data = bytes(r2.cmdj('pxj 0xbf4 @ 0x10004640'))
 
-print(encrypted_data[:4][::-1].hex())
 unencrypted = b''
 key = 0x6949
 
-
 for i in range(0,len(encrypted_data)-4,4):
-    #print(encrypted_data[i:i+4].hex())
+    # Grab dword from encrypted data and convert into integer
     d = int(encrypted_data[i:i+4][::-1].hex(), 16)
     c = d ^ key
     c = rol(c, 4, 32)
     c += 0x77777778
     c = c & 0xffffffff
+    # Put the newly decrypted data into the empty buffer
     unencrypted += struct.pack('<I',c)
 
 f = open("out.bin", 'wb')
--- unpack_sample.py
+++ unpack_sample.py
@@ -1,6 +1,7 @@
 import r2pipe
 import struct
 
+# Global variables used in the check function
 var_1 = 0
 var_2 = 0
 
@@ -12,6 +13,15 @@ def tohex(x):
   return (x + (1 << 32)) % (1 << 32)
 
 def check(data, i):
+    """Peforms check based on current index of data
+
+    Arguments:
+        data -- The data that we are performing the check on
+        i -- The current index of the data that we are looping through
+
+    Returns:
+        tuple of value a and modified i
+    """
     global var_1
     global var_2
     a = var_1
@@ -26,6 +36,15 @@ def check(data, i):
     return (b, i)
 
 def check2(dat, i):
+    """Performs check based on current index using the previous check function
+
+    Arguments:
+        dat -- The data we are performing the check on
+        i -- The current index of the data we are looping through
+
+    Returns:
+        tuple of value a and modified i
+    """
     var_4h = 1
     while True:
         (a, i) = check(dat, i)
@@ -34,126 +53,159 @@ def check2(dat, i):
         if a == 0:
             break
     return (var_4h, i)
- 
-r2 = r2pipe.open("mal1.dll")
 
-encrypted_data = bytes(r2.cmdj('pxj 0x3c870 @ 0x10005238'))
+def compress_data(data):
+    """Compresses data
 
-print(encrypted_data[:4])
-compressed_data = b''
+    Arguments:
+        data -- The data to be compressed
 
-x = 0
-while x < len(encrypted_data):
-    if x % 2 == 0:
-        x += 2
-    compressed_data += bytes([encrypted_data[x]])
-    x += 1
-    
-encrypted_data = compressed_data
-print(encrypted_data[:4])
+    Returns:
+        byte array of compressed data
+    """
+    compressed_data = b''
 
-unencrypted = b''
-key = 0x4178
+    x = 0
+    while x < len(data):
+        if x % 2 == 0:
+            x += 2
+        compressed_data += bytes([data[x]])
+        x += 1
+    return compressed_data
 
-for i in range(0,len(encrypted_data)-4,4):
-    #print(encrypted_data[i:i+4].hex())
-    d = int(encrypted_data[i:i+4][::-1].hex(), 16)
-    c = d ^ key
-    c = rol(c, 4, 32)
-    c += 0x77777778
-    c = c & 0xffffffff
-    unencrypted += struct.pack('<I',c)
 
-f = open("out2.bin", 'wb')
-f.write(unencrypted)
-f.close()
+def dword_decrypt(data, key):
+    """Performs an xor decryption on each dword in the data provided
 
-encrypted = list(unencrypted)
-decrypted = [0 for x in range(len(encrypted))]
-decrypted[0] = encrypted[0]
-dec_i = 1
-enc_i = 1
-var_ch = -1
-var_4h = 0
-end = False
-while not end:
-    (a, enc_i) = check(encrypted, enc_i)
-    if a != 0:
-        (a, enc_i) = check(encrypted, enc_i)
+    Arguments:
+        data -- The data to decrypt
+        key -- The key used to decrypt the data
+
+    Returns:
+        byte array of the decrypted data
+    """
+    unencrypted = b''
+
+    for i in range(0,len(data)-4,4):
+        d = int(data[i:i+4][::-1].hex(), 16)
+        c = d ^ key
+        c = rol(c, 4, 32)
+        c += 0x77777778
+        c = c & 0xffffffff
+        unencrypted += struct.pack('<I',c)
+    return unencrypted
+
+
+def deobfuscation(data):
+    """Deobfuscates data using the method used in the Get2 downloader
+
+    Arguments:
+        data -- a list of integers to be deobfuscated
+
+    Returns:
+        a byte array of the deobfuscated data
+    """
+    decrypted = [0 for x in range(len(data))]
+    decrypted[0] = data[0]
+    dec_i = 1
+    enc_i = 1
+    var_ch = -1
+    var_4h = 0
+    end = False
+    while not end:
+        (a, enc_i) = check(data, enc_i)
         if a != 0:
-            (a, enc_i) = check(encrypted, enc_i)
+            (a, enc_i) = check(data, enc_i)
             if a != 0:
-                var_14h = 0
-                var_8h = 4
-                while var_8h != 0:
-                    (a, enc_i) = check(encrypted, enc_i)
-                    var_14h = a + (var_14h*2)
-                    var_8h -= 1
-                if var_14h == 0:
-                    decrypted[dec_i] = 0
-                    dec_i += 1
+                (a, enc_i) = check(data, enc_i)
+                if a != 0:
+                    var_14h = 0
+                    var_8h = 4
+                    while var_8h != 0:
+                        (a, enc_i) = check(data, enc_i)
+                        var_14h = a + (var_14h*2)
+                        var_8h -= 1
+                    if var_14h == 0:
+                        decrypted[dec_i] = 0
+                        dec_i += 1
+                    else:
+                        decrypted[dec_i] = decrypted[dec_i-var_14h]
+                        dec_i += 1
+                    var_4h = 0
                 else:
-                    decrypted[dec_i] = decrypted[dec_i-var_14h]
-                    dec_i += 1
-                var_4h = 0
+                    var_14h = data[enc_i]
+                    enc_i += 1
+                    var_10h = (var_14h & 1) + 2
+                    var_14h = var_14h >> 1
+                    if var_14h == 0:
+                        end = True
+                        var_ch = var_14h
+                        var_4h = 1
+                    else:
+                        while var_10h != 0:
+                            decrypted[dec_i] = decrypted[dec_i-var_14h]
+                            dec_i += 1
+                            var_10h -= 1
+                        var_ch = var_14h
+                        var_4h = 1
+
             else:
-                var_14h = encrypted[enc_i]
-                enc_i += 1
-                var_10h = (var_14h & 1) + 2
-                var_14h = var_14h >> 1
-                if var_14h == 0:
-                    end = True
-                    var_ch = var_14h
+                (a, enc_i) = check2(data, enc_i)
+                var_14h = a
+                if var_4h == 0 and var_14h == 2:
+                    var_14h = var_ch
+                    (a, enc_i) = check2(data, enc_i)
+                    var_10h = a
+                    while var_10h != 0:
+                        decrypted[dec_i] = decrypted[dec_i-var_14h]
+                        dec_i += 1
+                        var_10h -= 1
                     var_4h = 1
                 else:
+                    if var_4h == 0:
+                        var_14h -= 3
+                    else:
+                        var_14h -= 2
+                    var_14h = var_14h << 8
+                    var_14h = data[enc_i] + var_14h
+                    enc_i += 1
+                    (a, enc_i) = check2(data, enc_i)
+                    var_10h = a
+                    if var_14h >= 0x7d00:
+                        var_10h += 1
+                    if var_14h >= 0x500:
+                        var_10h += 1
+                    if var_14h < 0x80:
+                        var_10h += 2
                     while var_10h != 0:
                         decrypted[dec_i] = decrypted[dec_i-var_14h]
                         dec_i += 1
                         var_10h -= 1
                     var_ch = var_14h
                     var_4h = 1
-
         else:
-            (a, enc_i) = check2(encrypted, enc_i)
-            var_14h = a
-            if var_4h == 0 and var_14h == 2:
-                var_14h = var_ch
-                (a, enc_i) = check2(encrypted, enc_i)
-                var_10h = a
-                while var_10h != 0:
-                    decrypted[dec_i] = decrypted[dec_i-var_14h]
-                    dec_i += 1
-                    var_10h -= 1
-                var_4h = 1
-            else:
-                if var_4h == 0:
-                    var_14h -= 3
-                else:
-                    var_14h -= 2
-                var_14h = var_14h << 8
-                var_14h = encrypted[enc_i] + var_14h
-                enc_i += 1
-                (a, enc_i) = check2(encrypted, enc_i)
-                var_10h = a
-                if var_14h >= 0x7d00:
-                    var_10h += 1
-                if var_14h >= 0x500:
-                    var_10h += 1
-                if var_14h < 0x80:
-                    var_10h += 2
-                while var_10h != 0:
-                    decrypted[dec_i] = decrypted[dec_i-var_14h]
-                    dec_i += 1
-                    var_10h -= 1
-                var_ch = var_14h
-                var_4h = 1
-    else:
-        decrypted[dec_i] = encrypted[enc_i]
-        dec_i += 1
-        enc_i += 1
-        var_4h = 0
+            decrypted[dec_i] = data[enc_i]
+            dec_i += 1
+            enc_i += 1
+            var_4h = 0
+    decrypted = bytes(decrypted)
+    return decrypted
 
-decrypted = bytes(decrypted)
-f = open("payload.bin", "wb")
-f.write(decrypted)
-f.close()
+
+if __name__ == '__main__':
+    r2 = r2pipe.open("mal1.dll")
+
+    encrypted_data = bytes(r2.cmdj('pxj 0x3c870 @ 0x10005238'))
+
+    compressed_data = compress_data(encrypted_data)
+    unencrypted = dword_decrypt(encrypted_data, 0x4178)
+
+    f = open("out2.bin", 'wb')
+    f.write(unencrypted)
+    f.close()
+
+    encrypted = list(unencrypted)
+    decrypted = deobfuscation(encrypted)
+    f = open("payload.bin", "wb")
+    f.write(decrypted)
+    f.close()
